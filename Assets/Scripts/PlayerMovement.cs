2def20655c97376221472d84ecc54f5fff20f75f
Alex <akharoukpp@gmail.com>
2019-07-27 10:47:15 +0100

    original commit

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private const string Message = "Welcome to the Fundamentals!";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log(Message);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
