7b2b4148b7d44cbfb563b1bae9d26de83e92d69e
Alex <akharoukpp@gmail.com>
2019-07-27 12:40:51 +0100

    add collision and detection

---
+++ Assets/Scripts/PlayerCollision.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log($"We hit the {collision.collider.name.ToLower()}.");
+
+        if (collision.collider.name == "Obstacle")
+        {
+            Debug.Log("Game Over.");
+        }
+    }
+}
