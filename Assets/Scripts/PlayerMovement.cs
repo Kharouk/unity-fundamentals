1009df05fd679d4117eb2c066905b533bb69bf89
Alex <akharoukpp@gmail.com>
2019-07-27 12:10:17 +0100

    Refactor movement to be more accesible

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,8 +6,8 @@ public class PlayerMovement : MonoBehaviour
 {
     public Rigidbody RB;
     public float forwardForce = 2000f; // default to 2000. 
-    public float leftForce = 500f;
-    public float rightForce = -500f;
+    public float xAxisForce = 500f;
+    public float xAxisInput;
 
 
     private const string Message = "Welcome to the Fundamentals!"; // Doesn't appear on Unity.
@@ -17,7 +17,6 @@ public class PlayerMovement : MonoBehaviour
     {
         Debug.Log(Message);
         RB = GameObject.Find("Player").GetComponent<Rigidbody>(); // Allows us to automatically assign the correct RigidBody
-        Debug.Log(RB);
     }
 
     // Update is called once per frame 
@@ -30,16 +29,20 @@ public class PlayerMovement : MonoBehaviour
         */
         RB.AddForce(0, 0, stabilizeSpeed(forwardForce));
 
-        if ( Input.GetKey("d"))
-        {
-            Debug.Log("You've hit right.");
-            RB.AddForce(stabilizeSpeed(leftForce), 0, 0);
-        }
-        if ( Input.GetKey("a"))
-        {
-            Debug.Log("You've hit left.");
-            RB.AddForce(stabilizeSpeed(rightForce), 0, 0);
-        }
+        xAxisInput = Input.GetAxis("Horizontal") * xAxisForce;
+        RB.AddForce(stabilizeSpeed(xAxisInput), 0, 0);
+
+        // OR:
+//         if (Input.GetKey("d") || Input.GetKey(KeyCode.RightArrow))
+//         {
+//             Debug.Log("You've hit right.");
+//             RB.AddForce(stabilizeSpeed(xAxisForce), 0, 0);
+//         }
+//         if (Input.GetKey("a") || Input.GetKey(KeyCode.LeftArrow))
+//         {
+//             Debug.Log("You've hit left.");
+//             RB.AddForce(stabilizeSpeed(-xAxisForce), 0, 0);
+//         }
     }
 
     float stabilizeSpeed(float num)
